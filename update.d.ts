2598d2f36c0d866d069a2cc1c981512b43fc49d5
Selali Adobor <selaliadobor@Realitys-MacBook-Pro.local>
2017-08-07 14:27:08 -0400

    Add support for extension files
    Extension files allow for reduced authentication overhead

--- update.d.ts
+++ update.d.ts
@@ -1,4 +1,5 @@
 declare class Update {
     static extractAsset(path: string, destinationPath: string);
+    static extractExtension(extension: string, path: string, destinationPath: string)
     static getTransferDirectoryRelativePath(path: string): string;
 }
